d14227c2bac008feda5fec0d331f45b0e7844f47
rafaelpv <rafaelpviegas@gmail.com>
2015-09-19 02:48:32 -0300

    Tela profile
    Correções Travis.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,29 +11,28 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    public function update($userId, Request $request)
+    {
+        $user = User::findOrFail($userId);
 
-	public function update($id, Request $request)
-	{
-		$user = User::findOrFail($id);
-	
-		$this->validate($request, [
-				'name' => 'required',
-				'email' => 'required'
-				]);
-	
-		$input = $request->all();
-	
-		$user->fill($input)->save();
-	
-		$request->session()->flash('flash_message', 'Altera&ccedil;&otilde;es salvas com sucesso!');
-	
+        $this->validate($request, [
+                'name' => 'required',
+                'email' => 'required'
+                ]);
+
+        $input = $request->all();
+
+        $user->fill($input)->save();
+
+        $request->session()->flash('flash_message', 'Altera&ccedil;&otilde;es salvas com sucesso!');
+
 		return redirect()->back();
-	}
-	
+    }
+
     public function showProfile()
     {
-    	$task = User::findOrFail(Auth::id());
-    	
-    	return view('profile')->withUser($task);
+        $task = User::findOrFail(Auth::id());
+
+        return view('profile')->withUser($task);
     }
 }
