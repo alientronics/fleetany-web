9cd9288106f3c9e3fa4705e475d1ed62837e5ab8
Rafael <rafaelpviegas@gmail.com>
2016-02-10 16:39:31 -0200

    #9 Adição de teste funcional para cadastro e edição de usuários

---
+++ tests/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testUser()
+    {
+        $this->be(User::find(1));
+
+        $this->visit('/user/create')
+            ->type('Nome Usuario Teste', 'name')
+            ->type('email@usuario.com', 'email')
+            ->type('123456', 'password')
+            ->type('Contato Usuario Teste', 'contact_id')
+            ->type('Empresa Usuario Teste', 'company_id')
+            ->press('Enviar')
+            ->seePageIs('/user')
+        ;
+        
+        $this->seeInDatabase('users', ['name' => 'Nome Usuario Teste', 'email' => 'email@usuario.com']);
+        
+        
+        $this->click('Nome Usuario Teste')
+            ->type('Nome Usuario Editado', 'name')
+            ->type('emaileditado@usuario.com', 'email')
+            ->type('654321', 'password')
+            ->type('Contato Usuario Editado', 'contact_id')
+            ->type('Empresa Usuario Editado', 'company_id')
+            ->press('Enviar')
+        ;
+
+        $this->seeInDatabase('users', ['name' => 'Nome Usuario Editado', 'email' => 'emaileditado@usuario.com']);
+    }
+}
