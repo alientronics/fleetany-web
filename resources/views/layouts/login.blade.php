99fa941cc22fe934e0791b1b89c8a42c09a6111b
Henrique Dias <heukirne@gmail.com>
2016-02-13 12:09:14 -0200

    Fix assets reference at login layout

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -20,16 +20,16 @@
     </div>
 
     <!-- jQuery -->
-    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="{{URL::asset('js/jquery.min.js')}}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="{{URL::asset('js/metisMenu.min.js')}}"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
+    <script src="{{URL::asset('js/sb-admin-2.js')}}"></script>
 
 </body>
 
