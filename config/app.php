a67bd85deebd46f8ff01c56e3d69b6a8a05db4d1
Henrique Santos <henrique@Henriques-Mac-mini.local>
2016-02-16 10:41:58 -0200

    corrige classe HTML #10

--- config/app.php
+++ config/app.php
@@ -194,11 +194,10 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-		'Form'      => Illuminate\Html\FormFacade::class,
-        'HTML'      => Illuminate\Html\HtmlFacade::class,
 		'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 
     ],
 
