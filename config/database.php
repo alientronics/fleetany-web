a334d60bb6d440bfbbdf0c8206a81a539cb6fcb2
Henrique Santos <henrique@Henriques-Mac-mini.local>
2016-02-27 09:32:54 -0300

    fix travis and codeclimate

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => env('DB_PREFIX', ''),,
+            'prefix'    => env('DB_PREFIX', ''),
             'strict'    => false,
         ],
 
