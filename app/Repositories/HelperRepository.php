826ec2c51e079bc6b4d54b51ab2d834f4557cd65
Rafael <rafaelpviegas@gmail.com>
2016-03-02 18:58:42 -0300

    Correções do Travis

--- app/Repositories/HelperRepository.php
+++ app/Repositories/HelperRepository.php
@@ -3,10 +3,11 @@
 namespace App\Repositories;
 
 use Illuminate\Http\Request;
+
 class HelperRepository
 {
-    public function getFilters($form, $fields, Request $request) {
-        
+    public function getFilters($form, $fields, Request $request)
+    {
         $filters = $this->getFiltersValues($form, $fields);
         $filters['sort_url'] = $filters;
         unset($filters['sort_url']['sort']);
@@ -15,14 +16,14 @@ class HelperRepository
         $filters['paginate'] = empty($form['paginate']) ? $paginate : $form['paginate'];
         $request->session()->put('paginate', $filters['paginate']);
         
-        if(empty($form['sort'])) {
+        if (empty($form['sort'])) {
             $filters['sort'] = $fields[0];
             $filters['order'] = 'asc';
         } else {
             $sort = explode("-", $form['sort']);
             $filters['order'] = array_pop($sort);
             $filters['sort'] = implode("-", $sort);
-            if(!in_array($filters['sort'], $fields)){
+            if (!in_array($filters['sort'], $fields)) {
                 $filters['sort'] = $fields[0];
             }
         }
@@ -33,9 +34,10 @@ class HelperRepository
         return $filters;
     }
     
-    private function getFiltersValues($form, $fields) {
+    private function getFiltersValues($form, $fields)
+    {
         $filters = array();
-        if(!empty($fields)) {
+        if (!empty($fields)) {
             foreach ($fields as $value) {
                 $filters[$value] = empty($form[$value]) ? "" : $form[$value];
             }
@@ -43,17 +45,18 @@ class HelperRepository
         return $filters;
     }
     
-    private function getFiltersSortUrl($filters, Request $request) {
+    private function getFiltersSortUrl($filters, Request $request)
+    {
         $sortUrl = http_build_query($filters['sort_url']);
-        foreach(array_keys($filters['sort_url']) as $key) {
+        foreach (array_keys($filters['sort_url']) as $key) {
             $url = $request->path() . '?' . $sortUrl . "&sort=" . $key;
-            if($filters['sort'] == $key && $filters['order'] == 'asc') {
+            if ($filters['sort'] == $key && $filters['order'] == 'asc') {
                 $filters['sort_url'][$key] = $url . "-desc";
                 $filters['sort_icon'][$key] = "fa-sort-asc";
             } else {
                 $filters['sort_url'][$key] = $url . "-asc";
                 $filters['sort_icon'][$key] = "fa-sort";
-                if($filters['sort'] == $key) {
+                if ($filters['sort'] == $key) {
                     $filters['sort_icon'][$key] = "fa-sort-desc";
                 }
             }
