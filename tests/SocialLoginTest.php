d0b481e50ebdd5c886a87caf3baa641c378ff07d
Rafael <rafaelpviegas@gmail.com>
2016-03-07 14:58:39 -0300

    #13 Teste para o Social Login

---
+++ tests/SocialLoginTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class SocialLoginControllerTest extends TestCase
+{
+
+    public function testSocialLogin()
+    {
+    	$user = factory(App\User::class)->create();
+
+        $mockSocialite = Mockery::mock('Laravel\Socialite\Contracts\Factory');
+        $this->app->instance('Laravel\Socialite\Contracts\Factory', $mockSocialite);
+        $mockSocialDriver = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+
+        $mockSocialite->shouldReceive('driver')->twice()->with('google')->andReturn($mockSocialDriver);
+        $mockSocialDriver->shouldReceive('redirect')->once()->andReturn(redirect('/'));
+        $mockSocialDriver->shouldReceive('user')->once()->andReturn($user);
+
+        $this->visit('/auth/social/google');
+        $this->visit('/auth/google/callback');
+
+        $this->seeInDatabase('users', ['name' => $user->name, 'email' => $user->email]);
+    }
+
+}
