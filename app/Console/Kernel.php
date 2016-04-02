70a630ff135fa23018fb3ec5fcde1dac5376dd25
Rafael <rafaelpviegas@gmail.com>
2016-04-01 23:46:38 -0300

    Tentativa de corrigir Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -15,13 +14,4 @@ class Kernel extends ConsoleKernel
     protected $commands = [
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
-    protected function schedule()
-    {
-    }
 }
