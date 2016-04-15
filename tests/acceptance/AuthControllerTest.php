c7ce925131f8a294c8b5f980e17bf774a8297b50
Rafael <rafaelpviegas@gmail.com>
2016-04-14 21:50:22 -0300

    Testes para criação de conta

--- tests/acceptance/AuthControllerTest.php
+++ tests/acceptance/AuthControllerTest.php
@@ -21,4 +21,44 @@ class AuthControllerTest extends AcceptanceTestCase
         
         $this->assertEquals(true, $this->isAuthenticated());
     }
+    
+    public function testCreateAccount()
+    {
+        $rememberToken = str_random(10);
+        $user = factory(\App\Entities\User::class)->create([
+            'remember_token' => $rememberToken
+        ]);
+        
+        $this->visit('/create-account/'.$rememberToken)
+            ->type('123456', 'password')
+            ->press('Log in')
+            ->seePageIs('/')
+            ->see('Seja bem-vindo')
+        ;
+    }
+    
+    public function testCreateAccountInvalidToken()
+    {
+        $rememberToken = str_random(10);
+        
+        $this->visit('/create-account/'.$rememberToken)
+            ->seePageIs('/auth/login')
+            ->see('O token utilizado')
+        ;
+    }
+    
+    public function testCreateAccountInvalidPassword()
+    {
+        $rememberToken = str_random(10);
+        $user = factory(\App\Entities\User::class)->create([
+            'remember_token' => $rememberToken
+        ]);
+        
+        $this->visit('/create-account/'.$rememberToken)
+            ->type('12345', 'password')
+            ->press('Log in')
+            ->seePageIs('/create-account/'.$rememberToken)
+            ->see('invalidpassword')
+        ;
+    }
 }
