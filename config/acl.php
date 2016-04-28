8413649d9dea7ee11055fca490933bfc905d4fed
Rafael <rafaelpviegas@gmail.com>
2016-04-27 21:50:33 -0300

    #27 Criação de cache para permissões de usuário

--- config/acl.php
+++ config/acl.php
@@ -8,7 +8,7 @@ return [
      * to package's model. You can define your model here.
      */
 
-    'role'       => 'Kodeine\Acl\Models\Eloquent\Role',
+    'role'       => 'Alientronics\CachedEloquentUser\Role',
     'permission' => 'Kodeine\Acl\Models\Eloquent\Permission',
 
     /**
