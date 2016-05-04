51ceecaf4249517f14bd6683d7739523b630f125
Rafael <rafaelpviegas@gmail.com>
2016-05-04 18:36:57 -0300

    #29 Correção Travis

--- app/Repositories/VehicleRepositoryEloquent.php
+++ app/Repositories/VehicleRepositoryEloquent.php
@@ -84,12 +84,15 @@ class VehicleRepositoryEloquent extends BaseRepository implements VehicleReposit
     {
         $sub = Gps::select(\DB::raw('MAX(id) as id'))
             ->groupBy('vehicle_id');
-                
+        
         $vehicles = Gps::select('gps.id', 'vehicle_id', 'latitude', 'longitude')
             ->join(\DB::raw("({$sub->toSql()}) as gps2"), 'gps.id', '=', 'gps2.id')
-            ->where('company_id', Auth::user()['company_id'])
-            ->groupBy('vehicle_id')
-            ->get();
+            ->where('company_id', Auth::user()['company_id']);
+        if (!empty($vehicle_id)) {
+            $vehicles = $vehicles->where('vehicle_id', $vehicle_id);
+        }
+        $vehicles = $vehicles->groupBy('vehicle_id')
+                            ->get();
         
         return $vehicles;
     }
