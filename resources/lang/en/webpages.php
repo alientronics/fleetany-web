5940d66e63bab5b9ac6e9a47e1703e6063088279
Rafael <rafaelpviegas@gmail.com>
2016-05-17 05:37:13 -0300

    Traduções para web pages

---
+++ resources/lang/en/webpages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'About'   => 'About',
+    'Home'   => 'Home',
+    'Pricing'   => 'Pricing',
+    'PrivacyPolicy'   => 'Privacy Policy',
+    'TermsOfService'   => 'Terms of Service'
+];
---
+++ resources/lang/pt-br/webpages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'About'   => 'Sobre',
+    'Home'   => 'In&iacute;cio',
+    'Pricing'   => 'Valores',
+    'PrivacyPolicy'   => 'Pol&iacute;tica de Privacidade',
+    'TermsOfService'   => 'Termos do Servi&ccedil;o'
+];
