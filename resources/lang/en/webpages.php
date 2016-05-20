512edbbce2a834aea53d593654546d34f782f985
Rafael <rafaelpviegas@gmail.com>
2016-05-20 19:04:00 -0300

    Exclusão das traduções do package de web pages

--- resources/lang/en/webpages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'About'   => 'About',
-    'Home'   => 'Home',
-    'Pricing'   => 'Pricing',
-    'PrivacyPolicy'   => 'Privacy Policy',
-    'TermsOfService'   => 'Terms of Service'
-];
--- resources/lang/pt-br/webpages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'About'   => 'Sobre',
-    'Home'   => 'In&iacute;cio',
-    'Pricing'   => 'Valores',
-    'PrivacyPolicy'   => 'Pol&iacute;tica de Privacidade',
-    'TermsOfService'   => 'Termos do Servi&ccedil;o'
-];
